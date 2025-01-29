b40b247d8403d10e2423cd066463c53695281883
sidkhaliddv <sidkhaliddv@gmail.com>
2025-01-29 23:56:23 +0500

    simple example of websockets in node js

---
+++ index.js
@@ -0,0 +1,25 @@
+import { server } from "websocket";
+import http from 'http'
+
+//create http server
+const httpServer = http.createServer();
+
+httpServer.listen(8080, ()=>{
+  console.log('server listening on 8080')
+});
+
+//create websocket server
+const wsServer = new server({
+  httpServer: httpServer,
+})
+
+wsServer.on('request', (req)=>{
+  const connection = req.accept();
+  console.log(new Date() + ' - Connection from origin - ' + req.origin + ' --accepted');
+  connection.on('message', message => {
+    console.log(`Received message ${message.utf8Data}`)
+  })
+  connection.on('close', ()=>{
+    console.log(`connection closed ${new Date}`)
+  })
+});
