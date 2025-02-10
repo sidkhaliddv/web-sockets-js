0620d35289f1d254a554b18152b2b68861f71c52
sidkhaliddv <sidkhaliddv@gmail.com>
2025-02-10 23:58:48 +0500

    maintained connections variable

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
 import { server } from "websocket";
 import http from 'http'
 
-//create http server
-const httpServer = http.createServer();
+let connections = 0
 
-httpServer.listen(8080, ()=>{
+//create http server
+const httpServer = http.createServer(function(req, res) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    'Content-Type': 'application/json'
+  }
+
+  let url = req.url;
+  if (url == '/') {
+    res.writeHead(200, headers)
+    res.write(JSON.stringify({app: process.env.APPID, connections}))
+    res.end()
+  }
+});
+
+httpServer.listen(8080, () => {
   console.log('server listening on 8080')
 });
 
@@ -15,14 +31,23 @@ const wsServer = new server({
 
 wsServer.on('request', (req)=>{
   const connection = req.accept();
+  connections++;
+  console.log('connections', connections)
+  setInterval(()=>{
+    connection.send(process.env.APPID || 'app1')
+  }, 10000)
   
   console.log(new Date() + ' - Connection on ' + process.env.APPID + ' from origin - ' + req.origin + ' --accepted ' + req.socket.remoteAddress + ' forwarded address');
 
   connection.on('message', message => {
+    // setInterval(()=>{
     connection.sendUTF(`Message Received. Message was ${message.utf8Data}`)
+    // }, 2000)
   })
   
   connection.on('close', ()=>{
+    connections--;
+    console.log('connections', connections)
     console.log(`connection closed ${new Date} from ${process.env.APPID}`)
   })
 
