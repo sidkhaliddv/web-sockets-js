1138494e86cc9a1740d3e4ad976eb97ad9c41be9
sidkhaliddv <sidkhaliddv@gmail.com>
2025-03-03 23:43:38 +0500

    added route for all connections at /all

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Simulator from './components/Simulator.tsx'
+import AllConnectionsInfo from './components/AllConnectionsInfo.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Simulator />} />
+        <Route path='/all' element = {<AllConnectionsInfo/>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 )
